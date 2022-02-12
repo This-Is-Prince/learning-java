88c39c93342701ad5730eba5536e63f56157cf32
Prince <this.is.kumar.prince@gmail.com>
2022-02-12 08:53:05 +0530

    first commit

---
+++ saurabh_shukla_java/lecture_3_first_program/FirstProgram.java
@@ -0,0 +1,9 @@
+package saurabh_shukla_java.lecture_3_first_program;
+
+public class FirstProgram{
+    public static void main(String []args){
+        System.out.println(
+            "First Program!"
+        );
+    }
+}
