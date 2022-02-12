a0a4eca9867bb3ae49ff296dfe38cd9f1a709baf
Prince <this.is.kumar.prince@gmail.com>
2022-02-12 09:26:32 +0530

    datatypes and keywords completed

---
+++ saurabh_shukla_java/lecture_4_data_types/DataTypesAndKeywords.java
@@ -0,0 +1,17 @@
+package saurabh_shukla_java.lecture_4_data_types;
+
+public class DataTypesAndKeywords {
+    public static void main(String[] args) {
+        System.out.println("-----Data Types and Keywords-----");
+        int counter = 1;
+        double temp = 3.14;
+        String name = "Prince Kumar";
+        int[] ages = new int[5];
+        char letters[] = new char[2];
+        System.out.println(counter);
+        System.out.println(temp);
+        System.out.println(name);
+        System.out.println(ages[0]);
+        System.out.println(letters[1]);
+    }
+}
