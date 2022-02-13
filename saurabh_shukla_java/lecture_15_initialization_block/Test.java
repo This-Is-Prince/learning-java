1e176b58696e5943ad003108ff3fb2b2456331e7
Prince <this.is.kumar.prince@gmail.com>
2022-02-13 11:36:05 +0530

    initialization block learned

---
+++ saurabh_shukla_java/lecture_15_initialization_block/Test.java
@@ -0,0 +1,26 @@
+package saurabh_shukla_java.lecture_15_initialization_block;
+
+public class Test {
+    private int x;
+    private static int k;
+
+    {
+        System.out.println("Initialization Block: x=" + x);
+        x = 5;
+    }
+    static {
+        System.out.println("Static Initialization Block: k=" + k);
+        k = 10;
+    }
+
+    public Test() {
+        System.out.println("Constructor: x=" + x);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("-----Initialization Block-----");
+        Test t1 = new Test();
+        Test t2 = new Test();
+        new Test();
+    }
+}
